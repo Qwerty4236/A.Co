8cb095be0f02593ef0b4fa235776010d0fd3d0f4
Qwerty4254 <ahmedhamdouni122@gmail.com>
2024-11-19 19:39:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const cursor=document.querySelector("div.cursor")
+const code=document.querySelector("div.code")
+const dna=document.querySelector("div.dna")
+const hbd=document.querySelector("div.hpb")
+
+code.addEventListener("mousemove",(e)=>{
+  gsap.to(cursor,.3,{
+    x:e.clientX,
+    y:e.clientY,
+  })
+})
+code.addEventListener("mouseenter",()=>{
+  gsap.to(cursor,.3,{
+    opacity:1,
+    scale:1,
+    rotation:0,
+    top:'-50px',
+    left:'-50px',
+  })
+})
+code.addEventListener("mouseleave",()=>{
+  gsap.to(cursor,.3,{
+    opacity:0,
+    scale:0,
+    rotation:-45,
+    top:'0',
+    left:'0',
+  })
+})
+
+dna.addEventListener("mousemove",(e)=>{
+  gsap.to(cursor,.3,{
+    x:e.clientX,
+    y:e.clientY,
+  })
+})
+dna.addEventListener("mouseenter",()=>{
+  gsap.to(cursor,.3,{
+    opacity:1,
+    scale:1,
+    rotation:0,
+    top:'-50px',
+    left:'-50px',
+  })
+})
+dna.addEventListener("mouseleave",()=>{
+  gsap.to(cursor,.3,{
+    opacity:0,
+    scale:0,
+    rotation:-45,
+    top:'0',
+    left:'0',
+  })
+})
+
+code.addEventListener('click',()=>{
+  window.location.href="./#"
+})
+dna.addEventListener('click',()=>{
+  window.location.href="./#"
+})
+hbd.addEventListener('click',()=>{
+  hbd.classList.toggle("bigf")
+})
