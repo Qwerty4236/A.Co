66153e914e6b1ff0ddd6bfc60a5271446a49b02c
Qwerty4254 <ahmedhamdouni122@gmail.com>
2024-11-19 20:20:03 +0100

    second comit

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const code=document.querySelector("div.code")
 const dna=document.querySelector("div.dna")
 const hbd=document.querySelector("div.hpb")
 
+window.addEventListener('resize',()=>{
+  if (window.innerWidth<=1000){
+    cursor.style.display='none'
+  }
+  else{
+    cursor.style.display='flex'
+  }
+},true)
+
+
 code.addEventListener("mousemove",(e)=>{
   gsap.to(cursor,.3,{
     x:e.clientX,
